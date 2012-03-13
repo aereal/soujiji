4c64898f80416e03e6ed42b9e5f2e365dbb83b46
aereal <aereal@kerare.org>
2012-03-13 22:16:44 +0900

    Install Rails 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soujiji::Application.config.secret_token = 'aa7bebc1bb8be364dadb038b224bc08b2323f0fc23d179380017199772cb8c4b840982a86af8d26cc1f1eb8e60b206d13268685c93914f49b0a097216edbf73c'
