7b0a12fb5185316d91573e9b22625424209d5b73
aereal <aereal@kerare.org>
2012-03-14 04:28:27 +0900

    Use Fabricator to generating Lang

--- spec/models/lang_spec.rb
+++ spec/models/lang_spec.rb
@@ -1,12 +1,12 @@
 describe Lang do
   context "when given empty name" do
-    subject { Lang.new(name: '') }
+    subject { Fabricate.build(:nameless_lang) }
 
     it { should_not be_valid }
   end
 
   context "when given some name" do
-    subject { Lang.new(name: 'Ruby') }
+    subject { Fabricate.build(:lang) }
 
     it { should be_valid }
   end
