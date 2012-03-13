02eeafbbe6ec1bdf990a0b1a2e46d27dcf3172e6
aereal <aereal@kerare.org>
2012-03-13 23:26:58 +0900

    Spork setup Rails well

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,15 @@
 #require 'rubygems'
-#require 'spork'
+require 'spork'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
-
 Spork.prefork do
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
