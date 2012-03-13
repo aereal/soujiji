9cd4313cad9386c9d5b8975caa5a60410abcfc64
aereal <aereal@kerare.org>
2012-03-14 03:44:19 +0900

    Add RSpec as test framwork to configurations

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,9 @@ module Soujiji
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture_replacement: true
+    end
   end
 end
