1085b3a0a75ea2e2a20fcc7aca3acb87e49d83f1
aereal <aereal@kerare.org>
2012-03-16 05:38:45 +0900

    Rename helper spec

--- spec/helpers/libraries_helper_spec.rb
+++ spec/helpers/libraries_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 # Specs in this file have access to a helper object that includes
-# the PackagesHelper. For example:
+# the LibrariesHelper. For example:
 #
-# describe PackagesHelper do
+# describe LibrariesHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       helper.concat_strings("this","that").should == "this that"
 #     end
 #   end
 # end
-describe PackagesHelper do
+describe LibrariesHelper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
