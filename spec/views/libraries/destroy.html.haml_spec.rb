63ed66dacc6bcec2d4c2d41b1d43356ea54c3aa9
aereal <aereal@kerare.org>
2012-03-16 05:10:34 +0900

    Rename view spec

--- spec/views/libraries/destroy.html.haml_spec.rb
+++ spec/views/libraries/destroy.html.haml_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe "packages/index.html.haml" do
+describe "libraries/destroy.html.haml" do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/views/libraries/index.html.haml_spec.rb
+++ spec/views/libraries/index.html.haml_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe "packages/update.html.haml" do
+describe "libraries/index.html.haml" do
   pending "add some examples to (or delete) #{__FILE__}"
 end
