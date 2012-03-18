71774daa599ae9139addcb07635a9bd11c0e0928
aereal <aereal@kerare.org>
2012-03-19 00:18:42 +0900

    Intern answer types

--- lib/forgery/forgeries/answer_type.rb
+++ lib/forgery/forgeries/answer_type.rb
@@ -1,10 +1,10 @@
 class Forgery::AnswerType < Forgery
   def self.valid
-    dictionaries[:answer_types].sample
+    dictionaries[:answer_types].sample.to_sym
   end
 
   def self.invalid
-    !dictionaries[:answer_types].include?(_type = Fogery::LoremIpsum.word) ?
-      _type : invalid
+    (!dictionaries[:answer_types].include?(_type = Forgery::LoremIpsum.word) ?
+      _type : invalid).to_sym
   end
 end
