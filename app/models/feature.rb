7ad166cb8c2015e07f36053d991e02d91023673d
aereal <aereal@kerare.org>
2012-03-19 00:20:06 +0900

    Feature#answer_type is enumerative

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,4 +2,5 @@ class Feature < ActiveRecord::Base
   belongs_to :topic
 
   validates :title, presence: true
+  validates :answer_type, inclusion: {in: [:boolean, :selectable, :enumerative]}
 end
--- spec/models/feature_spec.rb
+++ spec/models/feature_spec.rb
@@ -8,6 +8,30 @@ describe Feature do
   end
 
   describe 'with #title' do
+    it { should be_valid, subject.errors.inspect }
+  end
+
+  describe 'with #answer_type which is boolean' do
+    subject { Fabricate.build(:boolean_feature) }
+
     it { should be_valid }
+  end
+
+  describe 'with #answer_type which is selectable' do
+    subject { Fabricate.build(:selectable_feature) }
+
+    it { should be_valid }
+  end
+
+  describe 'with #answer_type which is enumerative' do
+    subject { Fabricate.build(:enumerative_feature) }
+
+    it { should be_valid }
+  end
+
+  describe 'with #answer_type which is not boolean, selectable, nor enumerative' do
+    subject { Fabricate.build(:invalid_answer_type_feature) }
+
+    it { should_not be_valid }
   end
 end
