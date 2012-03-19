608848fd2a4bda03ff2502332721dd56f84e8a84
aereal <aereal@kerare.org>
2012-03-20 03:03:59 +0900

    Add descriptive answer fabricator

--- spec/fabricators/answer_fabricator.rb
+++ spec/fabricators/answer_fabricator.rb
@@ -9,3 +9,8 @@ Fabricator :boolean_answer, from: :answer do
   type :boolean
   content { [true, false].sample }
 end
+
+Fabricator :descriptive_answer, from: :answer do
+  type :descriptive
+  content { Forgery::LoremIpsum.text }
+end
