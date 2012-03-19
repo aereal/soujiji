53779f10fc431588f72712bddecbdaad68062262
aereal <aereal@kerare.org>
2012-03-20 04:33:03 +0900

    Migrate to 20120319193217

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120319191302) do
+ActiveRecord::Schema.define(:version => 20120319193217) do
 
   create_table "answers", :force => true do |t|
     t.integer  "feature_id"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(:version => 20120319191302) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "language_id"
+    t.string   "slug"
   end
 
   create_table "topics", :force => true do |t|
