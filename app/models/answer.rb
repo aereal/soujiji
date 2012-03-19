e3641a43dfe50f2f7a4344bd5c7d7485eddcdd97
aereal <aereal@kerare.org>
2012-03-20 02:46:33 +0900

    Answer#type is boolean or descriptive

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,10 @@
 class Answer < ActiveRecord::Base
+  include Enumerize
+
+  enumerize :type, in: [:boolean, :descriptive]
+
   belongs_to :feature
   belongs_to :library
+
+  validates :type, inclusion: {in: %w(boolean descriptive)}
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,21 @@
-require 'spec_helper'
-
 describe Answer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Fabricate.build(:answer) }
+
+  describe 'with #type which is descriptive' do
+    before { subject.type = :descriptive }
+
+    it { should be_valid }
+  end
+
+  describe 'with #type which is boolean' do
+    before { subject.type = :boolean }
+
+    it { should be_valid }
+  end
+
+  describe 'with #type which is not boolean, nor descriptive' do
+    before { subject.type = Forgery::AnswerType.invalid }
+
+    it { should_not be_valid }
+  end
 end
